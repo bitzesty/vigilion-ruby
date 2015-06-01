1b89900682ae7b46fde293e2be9db6fe00794a62
Emiliano Ritiro <eritiro@gmail.com>
2015-06-01 09:26:16 -0300

    api_key presence validation

--- lib/vigilion/http.rb
+++ lib/vigilion/http.rb
@@ -3,35 +3,29 @@ module Vigilion
     attr_accessor :url, :options, :method
 
     def initialize
+      validate_api_key
       @conn = ::Faraday.new(url: Configuration.server_url) do |c|
         c.request :url_encoded
         c.response :json, content_type: /\bjson$/
         c.adapter ::Faraday.default_adapter
         c.headers = { "X-Auth-Token" => Configuration.api_key, "User-Agent" => "Vigilion #{Vigilion::VERSION} (#{RUBY_PLATFORM}, Ruby #{RUBY_VERSION})" }
       end
-    rescue Vigilion::HttpError => e
-      handle_exception(e)
     end
 
     def post(uuid, url)
       response = @conn.post "/scan", uuid: uuid, url: url
       response.body
-    rescue Vigilion::Error => e
-      handle_exception(e)
     end
 
     def get(uuid)
       response = @conn.get "/status/#{uuid}"
       response.body
-    rescue Vigilion::Error => e
-      handle_exception(e)
     end
 
-    private
+  private
 
-    def handle_exception(e)
-      puts e.message
-      puts e.backtrace.inspect
+    def validate_api_key
+      raise Vigilion::Error.new("api_key not present") unless Configuration.api_key
     end
   end
 end
